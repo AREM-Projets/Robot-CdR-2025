3b50c2563bc2a197685494c520a9e9f862072c0d
RasPi <elouan.marron@gmail.com>
2025-03-07 18:26:34 +0100

    mouvement de demo

--- Code/Raspi/main.py
+++ Code/Raspi/main.py
@@ -1,4 +1,5 @@
 import serial
+from time import sleep
 
 AVANCER                 = b'8'
 RECULER                 = b'2'
@@ -8,6 +9,25 @@ ROTATION_GAUCHE         = b'7'
 ROTATION_DROITE         = b'9'
 STOP                    = b'5'
 RESET_POS               = b'.'
+
+def getpos():
+    parse_error_flag = 1
+
+    while(parse_error_flag):
+        try:
+            port_embase.read()
+            sleep(0.1)
+            x = float(port_embase.readline().decode().split()[0].replace('\x00', ''))
+            y = float(port_embase.readline().decode().split()[1].replace('\x00', ''))
+            r = float(port_embase.readline().decode().split()[2].replace('\x00', ''))
+            
+
+            parse_error_flag = 0
+        except:
+            pass
+
+    return  (x, y, r)
+
 
 #connexion a l'embase
 try:
@@ -26,15 +46,16 @@ Sequence de deplacement
 
 Format de la trame de position envoyee par l'embase: "%.4f %.4f %.4f\n"
 """
-print("RESET_POS")
+
 port_embase.write(RESET_POS)
-print("AVANCER")
+while(getpos()[0] != 0): pass
+
 port_embase.write(AVANCER)
+while(getpos()[0] < 0.5) : pass
 
-# on attends d'avoir fait 0.1m selon le devant du robot au demarrage et on s'arrete
-print("Attente d'avoir atteint x=0.1m")
-while(float(port_embase.readline().split()[0]) < 0.1) : pass
-print("Position atteinte")
+port_embase.write(STOP)
 
-print("STOP")
+port_embase.write(RECULER)
+while(getpos()[0] > 0) : pass
+
 port_embase.write(STOP)
